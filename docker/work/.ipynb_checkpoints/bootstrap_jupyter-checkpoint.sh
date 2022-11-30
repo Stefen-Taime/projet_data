cac7b1fd5f26d63ca92a97526a8e6adcacd47192
Stefen-Taime <stefentaime@gmail.com>
2022-11-29 22:27:32 -0500

    Part-4: add spark-worker

---
+++ docker/work/.ipynb_checkpoints/bootstrap_jupyter-checkpoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+# update/upgrade and install htop
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install htop
+
+# install required python packages
+python3 -m pip install --user --upgrade pip
+python3 -m pip install -r requirements.txt --upgrade
+
+# download latest postgres driver jar
+POSTGRES_JAR="postgresql-42.2.22.jar"
+if [ -f "$POSTGRES_JAR" ]; then
+    echo "$POSTGRES_JAR exist"
+else
+    wget -nv "https://jdbc.postgresql.org/download/${POSTGRES_JAR}"
+fi
+
+# spark-submit logging level from INFO to WARN
+sudo cp log4j.properties /usr/local/spark/conf/log4j.properties
